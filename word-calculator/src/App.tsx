f3dbcbb4fed0ef28486dd2534e6e516cb83e0243
sagarkunwar <sagar.kunwar24@gmail.com>
2025-02-24 22:42:24 -0500

    word count calculator

--- word-calculator/src/App.tsx
+++ word-calculator/src/App.tsx
@@ -1,35 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState("");
+  const [wordCount, setWordCount] = useState(0);
+  const [characterCount, setCharacterCount] = useState(0);
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const handleInput = (e: { target: { value: any } }) => {
+    const input = e.target.value;
+    setCount(input);
+
+    const words = input.trim() ? input.trim().split(/\s+/).length : 0;
+    const characters = input.length;
+
+    setWordCount(words);
+    setCharacterCount(characters);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="card">
+      <div className="card-body">
+        <h1>Word Calculator</h1>
+        <input
+          type="text"
+          placeholder="Enter a word"
+          onChange={handleInput}
+          value={count}
+        />
+        <h2>Word Count: {wordCount}</h2>
+        <h2>Character Count: {characterCount}</h2>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
